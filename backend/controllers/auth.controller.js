3528df8525b109d07101cef950f2b35300797676
Terry Lu <u0509421@gmail.com>
2025-01-21 18:24:05 +0800

    login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -75,12 +75,36 @@ export const signup = async (req, res) => {
       message: "User created successfully",
     });
   } catch (error) {
+    console.log("Error in signup", error.message);
+
     res.status(500).json({ message: error.message });
   }
 };
 
 export const login = async (req, res) => {
-  res.send("Login route called");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = await generateTokens(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+      res.status(200).json({
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+        message: "Login successful",
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log("Error in login", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
@@ -97,6 +121,8 @@ export const logout = async (req, res) => {
     res.clearCookie("refreshToken");
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
+    console.log("Error in logout", error.message);
+
     res
       .status(500)
       .json({ message: "Internal server error", error: error.message });
